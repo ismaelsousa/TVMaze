b17f4028a41e81f8b9be2335c0e24b93233a0223
ismaelsousa <ismaelsousa1998@outlook.com>
2022-09-24 07:51:30 -0300

    feat: install react navigation

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+// import {Container} from './styles'
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
+};
+
+export default App;
